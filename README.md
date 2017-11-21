9c73b76346b45e0c2fa371e1a88b35b8c4f0df17
Lilly Piri <lilly@lillypiri.com>
2017-11-22 09:54:25 +1000

    added some notes

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 #### Project 9
 
-## Gallery App
+## Gallery App 🖼️
 
 ###### This is the ninth of twelve projects for the 🏡 Treehouse Techdegree Full Stack JavaScript.
 
 This project uses the [Flickr API](https://www.flickr.com/services/api/explore/flickr.photos.search) and React to retrieve images based on user searches. 
 
 `npm install` to install dependencies.
+`npm start` to run.
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // Display the images, if there are no results, let the user know.
+// Read about how to contruct a url here: https://www.flickr.com/services/api/misc.urls.html
 export default props => {
   if (!props.images || props.images.length === 0) return <div><h2>No results found</h2><p>That search did not return any results, please try again.</p></div>;
 
